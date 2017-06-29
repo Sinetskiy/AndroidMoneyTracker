6e78a59db9069f5e6c93399d23d9923019872ec1
Andrey Sinetskiy <sinetskiy@gmail.com>
2017-06-29 11:17:30 +0300

    leason 3 -added new classes for item

---
+++ app/src/main/java/moneytrackerjune17/loftschool/com/loftschoolmoneytrackerjune17/TempACtivity.java
@@ -0,0 +1,52 @@
+package moneytrackerjune17.loftschool.com.loftschoolmoneytrackerjune17;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class TempACtivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView add = (TextView) findViewById(R.id.add);
+        final EditText name = (EditText) findViewById(R.id.name);
+        final EditText price = (EditText) findViewById(R.id.price);
+        final TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                add.setEnabled(!TextUtils.isEmpty(name.getText())&&!TextUtils.isEmpty(price.getText()));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+
+        name.addTextChangedListener(textWatcher);
+        price.addTextChangedListener(textWatcher);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+}
